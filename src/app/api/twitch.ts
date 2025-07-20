3d17924793961593f3c00d441e0b0c07b768aae6
Max Gomes <maxgomes92@gmail.com>
2025-07-20 14:52:31 +0200

    feat(twitch): integration

---
+++ src/app/api/twitch.ts
@@ -0,0 +1,68 @@
+import { ApiClient, HelixStream, HelixUser } from "@twurple/api";
+import { AppTokenAuthProvider } from "@twurple/auth";
+
+class TwitchApi {
+  apiClient;
+  usersCache: Map<string, HelixUser> = new Map();
+  streamCache: Map<string, { stream: HelixStream; expireAt: number }> =
+    new Map();
+
+  constructor() {
+    const clientId = process.env.TWITCH_CLIENT_ID;
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
+    if (!clientId || !clientSecret) {
+      throw new Error("Missing TWITCH_CLIENT_ID and/or TWITCH_CLIENT_SECRET");
+    }
+
+    const authProvider = new AppTokenAuthProvider(clientId, clientSecret);
+    this.apiClient = new ApiClient({ authProvider });
+  }
+
+  async getUser(channelName: string) {
+    const userCache = this.usersCache.get(channelName);
+
+    if (userCache) {
+      console.warn(`[TwitchApi]: user from channel '${channelName}' from cache`);
+    }
+
+    const user =
+      userCache ?? (await this.apiClient.users.getUserByName(channelName));
+
+    if (!user) return null;
+
+    this.usersCache.set(channelName, user);
+
+    return user;
+  }
+
+  async getStream(channelName: string) {
+    const user = await this.getUser(channelName);
+
+    if (!user) {
+      return null;
+    }
+
+    const streamCache = this.streamCache.get(channelName);
+
+    if (streamCache && streamCache.expireAt > new Date().getTime()) {
+      console.warn(
+        `[TwitchApi]: stream from channel '${channelName}' from cache`,
+      );
+      return streamCache.stream;
+    }
+
+    const stream = await user.getStream();
+
+    if (stream) {
+      this.streamCache.set(channelName, {
+        expireAt: new Date().getTime() + 1000 * 60, // 1 minute
+        stream,
+      });
+    }
+
+    return stream;
+  }
+}
+
+export const twitchApi = new TwitchApi();
--- src/app/widgets/viewers/page.tsx
+++ src/app/widgets/viewers/page.tsx
@@ -1,23 +1,68 @@
-import { Icons } from "../../icons";
+import { twitchApi } from "@/app/api/twitch";
+import { Icons } from "@/app/icons";
+import { SearchParams } from "@/app/types";
+import { z } from "zod";
 
 const ICON_SIZE = 32;
 
-// Reference: /widgets/viewers/?platform=twitch&font=Arial&c=0101017f#ffffff
+// http://localhost:3000/widgets/viewers?y=gaules&t=gaules&k=gaules&c=0101017f&f=russo%20one
 
-export default function Widget() {
+const paramsScheme = z.object({
+  y: z.string(), // youtube channel
+  t: z.string(), // twitch channel
+  k: z.string(), // kick channel
+  c: z.string(), // color
+  f: z.enum([
+    "arial",
+    "times new roman",
+    "brush script mt",
+    "copperplate",
+    "helvética",
+    "verdana",
+    "georgia",
+    "decorative",
+    "impact",
+    "teko",
+    "russo one",
+  ]), // font-family
+});
+
+type Props = {
+  searchParams?: Promise<SearchParams>;
+};
+
+export default async function ViewersWidget({ searchParams }: Props) {
+  const params = paramsScheme.parse(await searchParams);
+  const counterStyle = {
+    fontFamily: params.f,
+  };
+
+  let twitchStream;
+
+  try {
+    twitchStream = params.t ? await twitchApi.getStream(params.t) : null;
+  } catch {
+    twitchStream = { viewers: -1 };
+  }
+
   return (
     <main className="flex flex-row justify-between">
       <Counter
         icon={<Icons.kick width={ICON_SIZE} height={ICON_SIZE} />}
-        viewers={3035}
-      />
-      <Counter
-        icon={<Icons.twitch width={ICON_SIZE} height={ICON_SIZE} />}
         viewers={1810}
+        style={counterStyle}
       />
+      {params.t && (
+        <Counter
+          icon={<Icons.twitch width={ICON_SIZE} height={ICON_SIZE} />}
+          viewers={twitchStream?.viewers || 0}
+          style={counterStyle}
+        />
+      )}
       <Counter
         icon={<Icons.youtube width={ICON_SIZE} height={ICON_SIZE} />}
         viewers={2060}
+        style={counterStyle}
       />
     </main>
   );
@@ -26,9 +71,13 @@ export default function Widget() {
 const Counter = ({
   icon,
   viewers,
+  style,
 }: {
   icon: React.ReactNode;
   viewers: number;
+  style: {
+    fontFamily: string;
+  };
 }) => (
   <div
     className="flex flex-row flex-wrap items-center gap-2 px-3 py-2 text-3xl"
@@ -36,7 +85,8 @@ const Counter = ({
       backgroundColor: "rgba(1, 1, 1, 0.498)",
       color: "white",
       fontWeight: "bold",
-      borderRadius: 16,
+      borderRadius: 14,
+      fontFamily: style.fontFamily,
     }}
   >
     {icon}
